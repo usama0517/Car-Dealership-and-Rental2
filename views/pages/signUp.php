a383b24f0bdd517923711fc75771559da2673ab5
Usama Abdurahman <120705497+usama0517@users.noreply.github.com>
2024-10-01 09:46:21 -0700

    Update signUp.php

--- views/pages/signUp.php
+++ views/pages/signUp.php
@@ -35,7 +35,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Log in</title>
+    <title>Sign Up</title>
     <link rel="stylesheet" href="./login.css" />
 </head>
 <body>
